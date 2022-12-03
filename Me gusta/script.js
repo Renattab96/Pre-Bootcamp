c087b8c68571ff603f4b242012331e2a996c28e4
Alexander Paniagua <renattabenitez@gmail.com>
2022-12-02 23:18:06 -0300

    Tarea de Me Gusta prioridad Core

--- Me gusta/script.js
+++ Me gusta/script.js
@@ -8,3 +8,36 @@ function add() {
     CountElement.innerText= count +' Likes'
 }
 
+
+// index 2 
+
+// metodo uno
+function addUno(){
+    let numero = document.querySelector("#number1");
+    console.log(numero);
+    let nuevoNumero = parseInt(numero.innerHTML);
+    nuevoNumero +=1;
+    numero.innerHTML = nuevoNumero;
+    
+}
+
+function addDos(){
+    let numero = document.querySelector("#number2");
+    console.log(numero);
+    let nuevoNumero = parseInt(numero.innerHTML);
+    nuevoNumero +=1;
+    numero.innerHTML = nuevoNumero;
+    
+}
+
+function addTres(){
+    let numero = document.querySelector("#number3");
+    console.log(numero);
+    let nuevoNumero = parseInt(numero.innerHTML);
+    nuevoNumero +=1;
+    numero.innerHTML = nuevoNumero;
+    
+}
+
+
+
