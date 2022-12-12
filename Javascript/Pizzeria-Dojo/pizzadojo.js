047750296007460a9dce93b43ae05ae25c6fa003
Renattab96 <renattabenitez96@gmail.com>
2022-12-12 19:36:27 -0300

    Pizzeria dojo Avance  de tarea Opcional

--- Javascript/Pizzeria-Dojo/pizzadojo.js
+++ Javascript/Pizzeria-Dojo/pizzadojo.js
@@ -1,82 +1,57 @@
-
-//  Una fábrica de sándwiches
-//  ¡En esta tarea haremos pizzas en JavaScript! Y si solo vamos a hacer una pizza, podemos definirla de una vez como el sándwich que vemos a continuación.
-
-
-// var sandwich = {
-//     pan:    "masa madre",
-//     proteína:  "asado",
-//     queso:   "queso suizo lacey",
-//     salsas: ["lechuga romana", "tomates reliquia", "salsa de rábano"]
-// };
+// Pizzería Dojo
+// Objetivos
+// Practicar la creación de objetos JavaScript
+// Practicar el uso deMath.random()
+// Una fábrica de sándwiches
+// ¡En esta tarea haremos pizzas en JavaScript! Y si solo vamos a hacer 
+// una pizza, podemos definirla de una vez como el sándwich que vemos a continuación.
+var sandwich = {
+    pan:    "masa madre",
+    proteína:  "asado",
+    queso:   "queso suizo lacey",
+    salsas: ["lechuga romana", "tomates reliquia", "salsa de rábano"]
+};
     
 // console.log(sandwich);
-//  Pero, ¿y si quisiéramos hacer varios sándwiches? ¡Podríamos definir una función que nos permita elaborar un sándwich!
+
+// Pero, ¿y si quisiéramos hacer varios sándwiches? ¡Podríamos definir una 
+// función que nos permita elaborar un sándwich!
+
+function sandwichFactory(pan, proteína, queso, salsas) {
+    var sandwich = {};
+    sandwich.pan = pan;
+    sandwich.proteína = proteína;
+    sandwich.queso = queso;
+    sandwich.salsas = salsas;
+    return sandwich;
+}
 
 
-// function sandwichFactory(pan, proteína, queso, salsas) {
-//     var sandwich = {};
-//     sandwich.pan = pan;
-//     sandwich.proteína = proteína;
-//     sandwich.queso = queso;
-//     sandwich.salsas = salsas;
-//     return sandwich;
-// }
-    
-// var s1 = sandwichFactory("trigo", "pavo", "provolone", ["mostaza", "cebolla frita", "rúcula"]);
-// console.log(s1);
-
+var s1 = sandwichFactory("trigo", "pavo", "provolone", ["mostaza", "cebolla frita", "rúcula"]);
+console.log(" Primer Sandwish preparado con una funcion a base de un objeto ",s1);
 // El horno de pizza
-// Crea una functionllamada pizzaOven.  Debemos sentirnos libres de personalizar la información que registramos para nuestra pizza, pero asegurémonos de incluir lo siguiente:  tipoCorteza, tipoSalsa, quesos,y salsas.
+// Crea una functionllamada pizzaOven.  Debemos sentirnos libres de personalizar la información 
+// que registramos para nuestra pizza, pero asegurémonos de incluir lo 
+// siguiente:  tipoCorteza, tipoSalsa, quesos,y salsas.
 
-// Crea una función llamada pizzaOven que devuelva un objeto JavaScript (Pizza)
+// let pizza = {
+//     pan : " ",
+//     quesos : " ",
+//     tipoCorteza : [" ", " ",],
+//     tipoSalsa : " ",
+// }
 
-// Crea una pizza con: "estilo Chicago", "tradicional", ["mozzarella"] y ["pepperoni", "salchicha"]
-
-// Crea una pizza con: "lanzada a mano" , "marinara" , ["mozzarella", "feta"] y ["champiñones", "aceitunas", "cebollas"]
-
-// ¡Crea 2 pizzas más con los ingredientes que quieras!
-function pizzaOven (chicago,tradicional,mozzarella ,pepperoni, salchicha) {
-    var pizza = {};
-    pizza.prepizza = prepizza,
-    pizza.decorado = decorado,
-    pizza.agregado = agregado,
-    pizza.salsas = salsas
+function  pizzaOven(panificado, quesitos, tipoCorteza, tipoSalsa){
+    let pizza = {};
+        pizza.pan = panificado;
+        pizza.quesos = quesitos;
+        pizza.tipoCorteza = tipoCorteza;
+        pizza.tipoSalsa = tipoSalsa;
     return pizza;
 }
-var pizza1 = pizzaOven( "marinara" , "mozzarella" ,"champiñones", "aceitunas")
-console.log(pizza1)
-var pizza2 = pizzaOven( "marinara" , "mozzarella" ,"champiñones", "aceitunas")
-console.log(pizza2)
-
-var chicago = {
-    prepizza: 'Cuadrada',
-    decorado:'Chorizo',
-    agregado:"Panceta ",
-    salsas:'Locote y Tomate ',
-}
-var pepperoni = {
-    prepizza: 'Cuadrada',
-    decorado:'Chorizo',
-    agregado:"Panceta ",
-    salsas:'tomate cebolla oregano',
-}
-
-var mozarella = {
-    prepizza: 'Cuadrada',
-    decorado:'alhabaca',
-    agregado:"Aceituna",
-    salsas:'tomate y cebolla',
-}
-var tradicional = {
-    prepizza: 'Cuadrada',
-    decorado:'alhabaca',
-    agregado:"Aceituna",
-    salsas:'tomate',
-}
-var salchicha = {
-    prepizza: 'Cuadrada',
-    decorado:'alhabaca',
-    agregado:"Aceituna",
-    salsas:'tomate cebolla oregano',
-}
+console.log("Primer pedido de pizza")
+let pizzaUno = pizzaOven("Cibatta", "mozzarella", ["verdura", "pepperoni"],["tomate", "cebolla", "oregano"])
+console.log(pizzaUno)
+console.log("Segundo pedido de pizza")
+let pizzaDos = pizzaOven("hecho a mano", "mozzarella", ["aceituna", "pepperoni", "cebolla"],["tomate", "cebolla", "oregano"])
+console.log(pizzaDos)
